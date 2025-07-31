d8f24698bb311af617d3a979af72f47b6275d4a4
hung <hung.reasearch@gmail.com>
2025-07-31 17:38:46 +0700

    update code #2

--- frontend/dist/js/app.js
+++ frontend/dist/js/app.js
@@ -1,1 +1,229 @@
-!function(){angular.module("taiga.plugins.notes",["ngRoute"]).config(["$routeProvider",function(t){return t.when("/notes",{templateUrl:"partials/notes.html",controller:"NotesListController"}).when("/notes/create",{templateUrl:"partials/note-edit.html",controller:"NoteEditPageController"}).when("/notes/:id/edit",{templateUrl:"partials/note-edit.html",controller:"NoteEditPageController"}).otherwise({redirectTo:"/notes"})}])}.call(this),function(){angular.module("taiga.plugins.notes").controller("NotesListController",["$scope","$location","$http",function(n,e,o){var r="http://127.0.0.1:8000/api";return n.notes=[],n.searchQuery="",n.loadNotes=function(){return o.get(r+"/notes/").then(function(t){return n.notes=t.data})},n.goToCreate=function(){return e.path("/notes/create")},n.editNote=function(t){return e.path("/notes/"+t.id+"/edit")},n.downloadNote=function(t){var n=new Blob([t.current_version_content||""],{type:"text/html"}),e=document.createElement("a");return e.href=URL.createObjectURL(n),e.download=`${t.name||"note"}.html`,e.click()},n.deleteNote=function(t){if(confirm(`Bạn có chắc muốn xóa ghi chú '${t.name}'?`))return o.delete(r+`/notes/${t.id}/`).then(function(){return n.loadNotes()})},n.getUpdatedAt=function(n){var t;return n.current_version_id&&n.versions&&(null!=(t=n.versions.find(function(t){return t.id===n.current_version_id}))?t.created_at:void 0)||n.created_at},n.loadNotes()}]),angular.module("taiga.plugins.notes").controller("NoteEditPageController",["$scope","$location","$timeout","$routeParams","$http",function(n,e,o,t,r){var i,c="http://127.0.0.1:8000/api",s=null!=(t=t.id)?t:null;return n.note={name:"",content:"",creator:1},n.versions=[],n.selectedVersion=null,t=function(){return null!=s?r.get(c+`/notes/${s}/`).then(function(t){return n.note=t.data,n.versions=n.note.versions||[],n.selectedVersion=n.versions.find(function(t){return t.id===n.note.current_version_id}),o(i,0)},function(t){return alert("Note không tồn tại hoặc lỗi server."),e.path("/notes")}):(n.versions=[],n.note={name:"",content:"",creator:1},n.selectedVersion=null,o(i,0))},n.selectVersion=function(t){if(n.selectedVersion=t,null!=window.editorInstance)return window.editorInstance.setData(t.content||"")},n.restoreSelectedVersion=function(){var t=n.selectedVersion;if(t&&t.id!==n.note.current_version_id)return r.post(c+`/notes/${s}/restore_version/`,{version_id:t.id}).then(function(t){if(n.note=t.data,n.versions=n.note.versions||[],n.selectedVersion=n.versions.find(function(t){return t.id===n.note.current_version_id}),null!=window.editorInstance)return window.editorInstance.setData(n.note.current_version_content||"")});alert("Vui lòng chọn version khác version hiện tại để phục hồi!")},i=function(){return null!=window.editorInstance&&window.editorInstance.destroy(),ClassicEditor.create(document.querySelector("#noteContent")).then(function(t){return(window.editorInstance=t).model.document.on("change:data",function(){return n.$$phase?n.note.content=t.getData():n.$apply(function(){return n.note.content=t.getData()})}),t.setData(n.note.current_version_content||"")}).catch(function(t){return console.error("CKEditor init failed:",t)})},n.save=function(){var t;return null!=window.editorInstance&&(n.note.content=window.editorInstance.getData()),t={name:n.note.name,content:n.note.content,creator:n.note.creator},null!=s?r.put(c+`/notes/${s}/`,t).then(function(){return e.path("/notes")}):r.post(c+"/notes/",t).then(function(t){return e.path("/notes")})},n.back=function(){return e.path("/notes")},n.$on("$destroy",function(){if(null!=window.editorInstance)return window.editorInstance.destroy(),window.editorInstance=null}),t()}])}.call(this);
+(function() {
+  angular.module('taiga.plugins.notes', ['ngRoute']).config([
+    '$routeProvider',
+    function($routeProvider) {
+      return $routeProvider.when('/notes',
+    {
+        templateUrl: 'partials/notes.html',
+        controller: 'NotesListController'
+      }).when('/notes/create',
+    {
+        templateUrl: 'partials/note-edit.html',
+        controller: 'NoteEditPageController'
+      }).when('/notes/:id/edit',
+    {
+        templateUrl: 'partials/note-edit.html',
+        controller: 'NoteEditPageController'
+      }).otherwise({
+        redirectTo: '/notes'
+      });
+    }
+  ]);
+
+}).call(this);
+
+(function() {
+  angular.module('taiga.plugins.notes').controller('NotesListController', [
+    '$scope',
+    '$location',
+    '$http',
+    function($scope,
+    $location,
+    $http) {
+      var API_BASE;
+      API_BASE = 'http://127.0.0.1:8000/api';
+      $scope.notes = [];
+      $scope.searchQuery = "";
+      $scope.loadNotes = function() {
+        return $http.get(`${API_BASE}/notes/`).then(function(res) {
+          return $scope.notes = res.data;
+        });
+      };
+      $scope.goToCreate = function() {
+        return $location.path('/notes/create');
+      };
+      $scope.editNote = function(note) {
+        return $location.path('/notes/' + note.id + '/edit');
+      };
+      $scope.downloadNote = function(note) {
+        var a,
+    blob;
+        blob = new Blob([note.current_version_content || ''],
+    {
+          type: 'text/html'
+        });
+        a = document.createElement("a");
+        a.href = URL.createObjectURL(blob);
+        a.download = `${note.name || 'note'}.html`;
+        return a.click();
+      };
+      $scope.deleteNote = function(note) {
+        if (confirm(`Bạn có chắc muốn xóa ghi chú '${note.name}'?`)) {
+          return $http.delete(`${API_BASE}/notes/${note.id}/`).then(function() {
+            return $scope.loadNotes();
+          });
+        }
+      };
+      // Hàm lấy ngày cập nhật mới nhất (của current_version)
+      $scope.getUpdatedAt = function(note) {
+        var ver;
+        if (note.current_version_id && note.versions) {
+          ver = note.versions.find(function(v) {
+            return v.id === note.current_version_id;
+          });
+          return (ver != null ? ver.created_at : void 0) || note.created_at;
+        } else {
+          return note.created_at;
+        }
+      };
+      return $scope.loadNotes();
+    }
+  ]);
+
+  angular.module('taiga.plugins.notes').controller('NoteEditPageController', [
+    '$scope',
+    '$location',
+    '$timeout',
+    '$routeParams',
+    '$http',
+    function($scope,
+    $location,
+    $timeout,
+    $routeParams,
+    $http) {
+      var API_BASE,
+    editorInit,
+    loadNoteAndVersions,
+    noteId,
+    ref;
+      API_BASE = 'http://127.0.0.1:8000/api';
+      noteId = (ref = $routeParams.id) != null ? ref : null;
+      $scope.note = {
+        name: "",
+        content: "",
+        creator: 1
+      };
+      $scope.versions = [];
+      $scope.selectedVersion = null;
+      loadNoteAndVersions = function() {
+        if (noteId != null) {
+          return $http.get(`${API_BASE}/notes/${noteId}/`).then(function(res) {
+            $scope.note = res.data;
+            $scope.versions = $scope.note.versions || [];
+            // Mặc định, chọn version hiện tại
+            $scope.selectedVersion = $scope.versions.find(function(v) {
+              return v.id === $scope.note.current_version_id;
+            });
+            return $timeout(editorInit,
+    0);
+          },
+    function(err) {
+            alert("Note không tồn tại hoặc lỗi server.");
+            return $location.path('/notes');
+          });
+        } else {
+          $scope.versions = [];
+          $scope.note = {
+            name: "",
+            content: "",
+            creator: 1
+          };
+          $scope.selectedVersion = null;
+          return $timeout(editorInit,
+    0);
+        }
+      };
+      // Hàm chọn version để preview
+      $scope.selectVersion = function(ver) {
+        $scope.selectedVersion = ver;
+        if (window.editorInstance != null) {
+          return window.editorInstance.setData(ver.content || "");
+        }
+      };
+      // Hàm phục hồi về version đang chọn
+      $scope.restoreSelectedVersion = function() {
+        var ver;
+        ver = $scope.selectedVersion;
+        if (!ver || ver.id === $scope.note.current_version_id) {
+          alert("Vui lòng chọn version khác version hiện tại để phục hồi!");
+          return;
+        }
+        return $http.post(`${API_BASE}/notes/${noteId}/restore_version/`,
+    {
+          version_id: ver.id
+        }).then(function(res) {
+          $scope.note = res.data;
+          $scope.versions = $scope.note.versions || [];
+          // Chọn lại version hiện tại mới nhất sau phục hồi
+          $scope.selectedVersion = $scope.versions.find(function(v) {
+            return v.id === $scope.note.current_version_id;
+          });
+          if (window.editorInstance != null) {
+            return window.editorInstance.setData($scope.note.current_version_content || "");
+          }
+        });
+      };
+      // Hàm khởi tạo CKEditor
+      editorInit = function() {
+        if (window.editorInstance != null) {
+          window.editorInstance.destroy();
+        }
+        return ClassicEditor.create(document.querySelector('#noteContent')).then(function(editor) {
+          window.editorInstance = editor;
+          editor.model.document.on('change:data',
+    function() {
+            if (!$scope.$$phase) {
+              return $scope.$apply(function() {
+                return $scope.note.content = editor.getData();
+              });
+            } else {
+              return $scope.note.content = editor.getData();
+            }
+          });
+          // Khi load lần đầu, editor hiển thị đúng content của version hiện tại
+          return editor.setData($scope.note.current_version_content || "");
+        }).catch(function(err) {
+          return console.error("CKEditor init failed:",
+    err);
+        });
+      };
+      // Lưu note (tạo/sửa)
+      $scope.save = function() {
+        var dataToSend;
+        if (window.editorInstance != null) {
+          $scope.note.content = window.editorInstance.getData();
+        }
+        dataToSend = {
+          name: $scope.note.name,
+          content: $scope.note.content,
+          creator: $scope.note.creator
+        };
+        if (noteId != null) {
+          return $http.put(`${API_BASE}/notes/${noteId}/`,
+    dataToSend).then(function() {
+            return $location.path('/notes');
+          });
+        } else {
+          return $http.post(`${API_BASE}/notes/`,
+    dataToSend).then(function(res) {
+            return $location.path('/notes');
+          });
+        }
+      };
+      $scope.back = function() {
+        return $location.path('/notes');
+      };
+      $scope.$on('$destroy',
+    function() {
+        if (window.editorInstance != null) {
+          window.editorInstance.destroy();
+          return window.editorInstance = null;
+        }
+      });
+      return loadNoteAndVersions();
+    }
+  ]);
+
+}).call(this);
+
+//# sourceMappingURL=app.js.map
--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -3,6 +3,7 @@ const coffee = require('gulp-coffee');
 const concat = require('gulp-concat');
 const sourcemaps = require('gulp-sourcemaps');
 const pug = require('gulp-pug');
+const browserSync = require('browser-sync').create();
 
 // Compile CoffeeScript → JS (Dev)
 gulp.task('coffee', function () {
@@ -11,28 +12,55 @@ gulp.task('coffee', function () {
         .pipe(coffee())
         .pipe(concat('app.js'))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('dist/js'));
+        .pipe(gulp.dest('dist/js'))
+        .pipe(browserSync.stream()); // BrowserSync reload JS
 });
 
 // Compile Jade (Pug) → HTML
 gulp.task('pug', function () {
     return gulp.src('src/templates/*.jade')
         .pipe(pug({ pretty: true }))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('dist'))
+        .pipe(browserSync.stream()); // BrowserSync reload HTML
 });
 
 // Compile partials (views)
 gulp.task('partials', function () {
     return gulp.src('src/templates/partials/*.jade')
         .pipe(pug({ pretty: true }))
-        .pipe(gulp.dest('dist/partials'));
+        .pipe(gulp.dest('dist/partials'))
+        .pipe(browserSync.stream());
 });
 
-// Watch task
-gulp.task('watch', function () {
+gulp.task('css', function () {
+    return gulp.src('src/styles/**/*.css')
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.stream());
+});
+
+
+// Serve + Watch task
+gulp.task('serve', function () {
+    // Khởi tạo server, thư mục gốc là dist
+    browserSync.init({
+        server: {
+            baseDir: './dist'
+        },
+        port: 3000,
+        open: false, // true nếu muốn tự mở browser khi start
+        notify: false
+    });
+
+    // Watch file, khi thay đổi sẽ chạy lại task và reload
     gulp.watch('src/coffee/**/*.coffee', gulp.series('coffee'));
-    gulp.watch('src/templates/**/*.jade', gulp.series('pug', 'partials'));
+    gulp.watch('src/templates/**.*.jade', gulp.series('pug', 'partials'));
+    gulp.watch('src/styles/**/*.css', gulp.series('css'));
+    gulp.watch('dist/*.html').on('change', browserSync.reload); // Reload khi HTML ngoài thay đổi
+
 });
 
-// Default task
-gulp.task('default', gulp.parallel('coffee', 'pug', 'partials'));
+// Default task: build 1 lần, rồi serve
+gulp.task('default', gulp.series(
+    gulp.parallel('coffee', 'pug', 'partials', 'css'),
+    'serve'
+));
